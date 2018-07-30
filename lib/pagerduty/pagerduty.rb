635e6367f16b002221015cb10b2b9086546e07a3
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2018-07-30 15:04:34 +0100

    remove params from incident

--- lib/pagerduty/pagerduty.rb
+++ lib/pagerduty/pagerduty.rb
@@ -330,7 +330,7 @@ class Pagerduty
   def incidents(options={})
 
     Pagerduty::Incidents.new(curl({
-      uri: "https://api.pagerduty.com/incidents?since=#{options[:since] || (Time.now - 1.day).strftime("%Y-%m-%d")}&until=#{options[:until] || (Time.now + 1.day).strftime("%Y-%m-%d")}",
+      uri: "https://api.pagerduty.com/incidents",
       params: {
         since: options[:since] || "",
         :until => options[:until] || "",
