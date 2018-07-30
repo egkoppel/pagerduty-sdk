bfc0d8fcb5205520aae5a5a4fe0c3f0b5b8ad6ee
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2018-07-30 11:44:05 +0100

    Add curl_with_headers function

--- lib/pagerduty/core.rb
+++ lib/pagerduty/core.rb
@@ -84,6 +84,66 @@ class Pagerduty
       end
     end
 
+    def curl_with_headers(options, headers)
+
+      curl_request = {
+        ssl: true,
+        port: 443,
+        headers: headers,
+      }
+
+      options.merge! curl_request
+
+      url = URI.parse(options[:uri])
+
+      if options[:params]
+        parameters = options[:params].map { |k,v| v.to_query(k) }.join("&")
+        url += "?#{parameters}"
+      end
+
+      http = Net::HTTP.new(url.host, 443)
+      http.use_ssl = true
+
+      request = case options[:method]
+                when 'DELETE'
+                  Net::HTTP::Delete.new(url)
+                when 'GET'
+                  Net::HTTP::Get.new(url)
+                when 'POST'
+                  Net::HTTP::Post.new(url)
+                when 'PUT'
+                  Net::HTTP::Put.new(url)
+                end
+
+      if options.has_key?(:data)
+        request.set_form_data(options[:data])
+      end
+
+      if options.has_key?(:basic_auth)
+        request.basic_auth options[:basic_auth][:user], options[:basic_auth][:password]
+      end
+
+      request.body = options[:body]
+
+      options[:headers].each { |key,val| request.add_field(key,val) }
+
+      response = if options[:method] == 'POST'
+                   http.post(url.path,options[:data].to_json,options[:headers])
+                 elsif options[:method] == 'PUT'
+                   http.put(url.path,options[:data].to_json,options[:headers])
+                 else
+                   http.request(request)
+                 end
+
+      if options[:raw_response] == true
+        response
+      elsif response.body
+        JSON.parse(response.body)
+      else
+        { 'code' => response.code, 'message' => response.message }
+      end
+    end
+
 
     # Check a Hash object for expected keys
     #
