cc6aae65006511b5a77914fdc953e3999405c27e
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2018-07-30 10:53:39 +0100

    Remove time range if not provided

--- lib/pagerduty/pagerduty.rb
+++ lib/pagerduty/pagerduty.rb
@@ -332,8 +332,8 @@ class Pagerduty
     Pagerduty::Incidents.new(curl({
       uri: "https://api.pagerduty.com/incidents",
       params: {
-        since: options[:since] || (Time.now - 1000.day).strftime("%Y-%m-%d"),
-        :until => options[:until] || (Time.now + 1.day).strftime("%Y-%m-%d"),
+        since: options[:since] || "",
+        :until => options[:until] || "",
       },
       method: 'GET'
     }))
