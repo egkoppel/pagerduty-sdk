ccab77e5a57d590f48d7fa69b8b31f82707dc20d
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2018-07-30 15:23:36 +0100

    Update incident definition

--- lib/pagerduty/models/incident.rb
+++ lib/pagerduty/models/incident.rb
@@ -6,20 +6,20 @@ class Pagerduty
 
       attribute :id
       attribute :incident_number
-      attribute :created_on
+      attribute :created_at
       attribute :status
       attribute :html_url
       attribute :incident_key
       attribute :service, Service
       attribute :escalation_policy, EscalationPolicy
-      attribute :assigned_to_user, AssignedUser
-      attribute :trigger_summary_data, TriggerSummaryData
-      attribute :trigger_details_html_url
-      attribute :trigger_type
-      attribute :last_status_change_on
-      attribute :last_status_change_by, LastStatusChangeBy
-      attribute :number_of_escalations
-      attribute :resolved_by_user, ResolvedByUser
+      #attribute :assigned_to_user, AssignedUser
+      #attribute :trigger_summary_data, TriggerSummaryData
+      #attribute :trigger_details_html_url
+      #attribute :trigger_type
+      #attribute :last_status_change_on
+      #attribute :last_status_change_by, LastStatusChangeBy
+      #attribute :number_of_escalations
+      #attribute :resolved_by_user, ResolvedByUser
       attribute :urgency
     end
 
