9469836a90c455c81f8384b9a0ad9f91ba597acc
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2018-07-30 11:47:49 +0100

    Update acknowledge and resolve to v2

--- lib/pagerduty/requests/incident.rb
+++ lib/pagerduty/requests/incident.rb
@@ -26,18 +26,44 @@ class Pagerduty
       end
 
       def acknowledge
-        curl({
-          uri: "https://#@@subdomain.pagerduty.com/api/v1/incidents/#{self.id}/acknowledge",
-          data: { 'requester_id' => self.assigned_to_user.id },
+        curl_with_headers({
+          uri: "https://api.pagerduty.com/incidents",
+          data: { 'payload' => "{
+            'incidents': [
+              {
+                'id':'#{self.id}',
+                'status':'acknowledged'
+              }
+            ]
+          }" },
           method: 'PUT'
+        },
+        {
+          "Content-Type" => "application/json",
+          "Authorization" => "Token token=#{Pagerduty.class_variable_get(:@@token)}",
+          "Accept" => "application/vnd.pagerduty+json;version=2",
+          "From" => self.assigned_to_user.email
         })
       end
 
       def resolve
-        curl({
-          uri: "https://#@@subdomain.pagerduty.com/api/v1/incidents/#{self.id}/resolve",
-          data: { 'requester_id' => self.assigned_to_user.id },
+        curl_with_headers({
+          uri: "https://api.pagerduty.com/incidents",
+          data: { 'payload' => "{
+            'incidents': [
+              {
+                'id':'#{self.id}',
+                'status':'resolved'
+              }
+            ]
+          }" },
           method: 'PUT'
+        },
+        {
+          "Content-Type" => "application/json",
+          "Authorization" => "Token token=#{Pagerduty.class_variable_get(:@@token)}",
+          "Accept" => "application/vnd.pagerduty+json;version=2",
+          "From" => self.assigned_to_user.email
         })
       end
 
